43024939f418fa852e4458b23c69be16ab40275e
ZyCromerZ <neetroid97@gmail.com>
2021-12-02 01:35:39 +0700

    update
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ for/begonia-r-oss-private.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+KernelBranch="r-oss-base-release-test"
+
+IncludeFiles "${MainPath}/device/begonia-r-oss.sh"
+CustomUploader="Y"
+# UseSpectrum="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
+# spectrumFile="None"
+FolderUp="begonia"
+TypeBuildTag="[Test]"
+
+CloneKernel "--depth=1"
+CloneZyCFoutTeenClang
+# CompileClangKernelB && CleanOut
+CloneCompiledGccEleven
+CompileClangKernel
+
+# cleanup stuff after done
+cd "${MainPath}"
+rm -rf *
