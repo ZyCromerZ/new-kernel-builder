4ccab9ee00811ac582a8aaf7618ddb8497eceb16
ZyCromerZ <neetroid97@gmail.com>
2022-02-14 12:29:38 +0700

    update release script
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- release-begonia.sh
+++ release-begonia.sh
@@ -2,3 +2,6 @@
 
 . pushg/vayu.sh doa begonia-q-oss-std-almk begonia-q-oss-std-b-almk begonia-q-oss-std-b-slmk begonia-q-oss-std-c-almk begonia-q-oss-std-c-slmk begonia-q-oss-std-d-almk begonia-q-oss-std-d-slmk begonia-q-oss-std-slmk begonia-q-oss-uv-almk begonia-q-oss-uv-b-almk begonia-q-oss-uv-b-slmk begonia-q-oss-uv-c-almk begonia-q-oss-uv-c-slmk begonia-q-oss-uv-d-almk begonia-q-oss-uv-d-slmk begonia-q-oss-uv-slmk && \
 . pushg/vayu.sh dob begonia-r-oss-std-almk begonia-r-oss-std-b-almk begonia-r-oss-std-b-slmk begonia-r-oss-std-c-almk begonia-r-oss-std-c-slmk begonia-r-oss-std-d-almk begonia-r-oss-std-d-slmk begonia-r-oss-std-slmk begonia-r-oss-uv-almk begonia-r-oss-uv-b-almk begonia-r-oss-uv-b-slmk begonia-r-oss-uv-c-almk begonia-r-oss-uv-c-slmk begonia-r-oss-uv-d-almk begonia-r-oss-uv-d-slmk begonia-r-oss-uv-slmk
+
+. pushg/vayu.sh doa begonia-q-oss-stock begonia-q-oss-stock-b begonia-q-oss-stock-c && \
+. pushg/vayu.sh dob begonia-r-oss-stock begonia-r-oss-stock-b begonia-r-oss-stock-c
