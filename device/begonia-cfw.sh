29752bbc7f7b0b00a908397db079aa158b3a6d09
ZyCromerZ <neetroid97@gmail.com>
2022-02-22 15:56:43 +0700

    test
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ device/begonia-cfw.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+DEVICE="Redmi Note 8 pro"
+CODENAME="Begonia"
+SaveChatID="-1001301538740"
+ARCH="arm64"
+TypeBuild="Stable"
+DEFFCONFIG="begonia_user_defconfig"
+GetBD=$(date +"%m%d")
+GetCBD=$(date +"%Y-%m-%d")
+# TypeBuildTag="[Q-OSS]"
+FolderUp="shared-file"
+ExFolder="Bego-CFW-kernels-release"
+AnyKernelBranch="master-begonia"
+FirstSendInfoLink="N"
+TypeBuildFor="CFW"
+
+# pullALmk(){
+#     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
+#     git reset --hard $HeadCommitId
+#     git fetch origin 20210205/main-ALMK2
+#     git pull --no-commit origin 20210205/main-ALMK2
+#     git commit -s -m 'Pull branch 20210205/main-ALMK2'
+#     TypeBuildTag="[Q-OSS][ALMK]"
+# }
+
+# pullSlmk(){
+#     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
+#     git reset --hard $HeadCommitId
+#     git fetch origin 20210205/main-SLMK2
+#     git pull --no-commit origin 20210205/main-SLMK2
+#     git commit -s -m 'Pull branch 20210205/main-SLMK2'
+#     TypeBuildTag="[Q-OSS][SLMK]"
+# }
+
+# PullLto(){
+#     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
+#     git reset --hard $HeadCommitId
+#     git fetch origin 20210205/main-LTO
+#     git pull --no-commit origin 20210205/main-LTO
+#     git commit -s -m 'Pull branch 20210205/main-LTO'
+#     TypeBuildTag="[Q-OSS][Stock-LMK][Thin-LTO]"
+# }
+
+# PullLtoALmk(){
+#     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
+#     git reset --hard $HeadCommitId
+#     git fetch origin 20210205/main-ALMK2-LTO
+#     git pull --no-commit origin 20210205/main-ALMK2-LTO
+#     git commit -s -m 'Pull branch 20210205/main-ALMK2-LTO'
+#     TypeBuildTag="[Q-OSS][ALMK][Thin-LTO]"
+# }
+
+# PullLtoSlmk(){
+#     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
+#     git reset --hard $HeadCommitId
+#     git fetch origin 20210205/main-SLMK2-LTO
+#     git pull --no-commit origin 20210205/main-SLMK2-LTO
+#     git commit -s -m 'Pull branch 20210205/main-SLMK2-LTO'
+#     TypeBuildTag="[Q-OSS][SLMK][Thin-LTO]"
+# }
---
+++ for/begonia-cfw.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+KernelBranch="cfw-main"
+
+IncludeFiles "${MainPath}/device/begonia-cfw.sh"
+CustomUploader="Y"
+# UseSpectrum="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
+# spectrumFile="bego-on-p.rc"
+FolderUp="shared-file"
+TypeBuildTag="[Test][ThinLTO][806Mhz]"
+
+CloneKernel "--depth=1"
+CloneCompiledGccTwelve
+CloneDTCClang
+# DisableMsmP
+# DisableThin
+EnableRELR
+CompileClangKernelLLVMB && CleanOut
