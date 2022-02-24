150c20f1ca0f04148e8578faf42c48900d272b29
ZyCromerZ <neetroid97@gmail.com>
2022-02-24 09:57:46 +0700

    begonia: drop stock kernel
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- release-begonia.sh
+++ release-begonia.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-. pushg/vayu.sh doa begonia-q-oss-std-c-almk begonia-q-oss-std-c-slmk begonia-q-oss-std-d-almk begonia-q-oss-std-d-slmk begonia-q-oss-uv-c-almk begonia-q-oss-uv-c-slmk begonia-q-oss-uv-d-almk begonia-q-oss-uv-d-slmk begonia-q-oss-stock begonia-q-oss-stock-b begonia-q-oss-stock-c && \
-. pushg/vayu.sh dob begonia-r-oss-std-c-almk begonia-r-oss-std-c-slmk begonia-r-oss-std-d-almk begonia-r-oss-std-d-slmk begonia-r-oss-uv-c-almk begonia-r-oss-uv-c-slmk begonia-r-oss-uv-d-almk begonia-r-oss-uv-d-slmk begonia-r-oss-stock begonia-r-oss-stock-b begonia-r-oss-stock-c
+. pushg/vayu.sh doa begonia-q-oss-std-c-almk begonia-q-oss-std-c-slmk begonia-q-oss-std-d-almk begonia-q-oss-std-d-slmk begonia-q-oss-uv-c-almk begonia-q-oss-uv-c-slmk begonia-q-oss-uv-d-almk begonia-q-oss-uv-d-slmk begonia-q-oss-uv-slmk begonia-q-oss-uv-almk begonia-q-oss-uv-b-slmk begonia-q-oss-uv-b-almk begonia-q-oss-std-slmk begonia-q-oss-std-almk begonia-q-oss-std-b-slmk begonia-q-oss-std-b-almk && \
+. pushg/vayu.sh dob begonia-r-oss-std-c-almk begonia-r-oss-std-c-slmk begonia-r-oss-std-d-almk begonia-r-oss-std-d-slmk begonia-r-oss-uv-c-almk begonia-r-oss-uv-c-slmk begonia-r-oss-uv-d-almk begonia-r-oss-uv-d-slmk begonia-r-oss-uv-slmk begonia-r-oss-uv-almk begonia-r-oss-uv-b-slmk begonia-r-oss-uv-b-almk begonia-r-oss-std-slmk begonia-r-oss-std-almk begonia-r-oss-std-b-slmk begonia-r-oss-std-b-almk
 
-# . pushd/vayu.sh zero # . pushd/vayu.sh zero begonia-r-oss-uv-slmk begonia-r-oss-uv-almk begonia-r-oss-uv-b-slmk begonia-r-oss-uv-b-almk begonia-r-oss-std-slmk
-# . pushd/vayu.sh zero begonia-r-oss-std-almk begonia-r-oss-std-b-slmk begonia-r-oss-std-b-almk begonia-q-oss-uv-slmk begonia-q-oss-uv-almk begonia-q-oss-uv-b-slmk
-# . pushd/vayu.sh zero begonia-q-oss-uv-b-almk begonia-q-oss-std-slmk begonia-q-oss-std-almk begonia-q-oss-std-b-slmk begonia-q-oss-std-b-almk
+#  begonia-q-oss-stock begonia-q-oss-stock-b begonia-q-oss-stock-c
+#  begonia-r-oss-stock begonia-r-oss-stock-b begonia-r-oss-stock-c
