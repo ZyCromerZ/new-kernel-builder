2e6bc1105bdd4654039517d2c8c53d917c7af0d2
ZyCromerZ <neetroid97@gmail.com>
2022-03-08 15:29:14 +0700

    ok
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- for/begonia-r-oss-private.sh
+++ for/begonia-r-oss-private.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-KernelBranch="r-oss-base-release-test"
+KernelBranch="r-oss-base-release"
 
 IncludeFiles "${MainPath}/device/begonia-r-oss.sh"
 CustomUploader="Y"
@@ -7,23 +7,12 @@ CustomUploader="Y"
 IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/begonia_kernel"
 # spectrumFile="None"
 FolderUp="shared-file"
-TypeBuildTag="[TEST]"
+TypeBuildTag="[806Mhz][r23p1]"
 
 CloneKernel "--depth=1"
-CloneZyCFifTeenClang
-# DisableThin
-OptimizaForPerf
-EnableRELR
-CompileClangKernelLLVM && CleanOut
-# CloneDTCClang
-# CloneCompiledGccTwelve
-# CompileClangKernelLLVMB && CleanOut
-# UseZyCLLVM="y"
-# TypeBuildTag="[TEST][ZyCLLVM]"
-# CompileClangKernelLLVMB && CleanOut
-CloneGCCOld
-CloneSdClang
+CloneCompiledGccTwelve
+CloneDTCClang
+# DisableMsmP
 # DisableThin
 EnableRELR
-OptimizaForPerf
 CompileClangKernelLLVMB && CleanOut
