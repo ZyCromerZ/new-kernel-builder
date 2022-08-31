e97cfc3ec2ced6aae875b8c6f7aab277fca07ffa
ZyCromerZ <neetroid97@gmail.com>
2022-08-31 17:38:57 +0700

    ok
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

---
+++ for/vayu-r-oss-f-ds.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+KernelBranch="20220412/main-x"
+
+IncludeFiles "${MainPath}/device/vayu-r-oss.sh"
+CustomUploader="Y"
+IncludeFiles "${MainPath}/misc/kernel.sh" "https://${GIT_SECRET}@github.com/${GIT_USERNAME}/vayu_kernel"
+# FolderUp="shared-file"
+TypeBuildTag="[MPDCL][FullLTO]"
+MultipleDtbBranch="$MultipleDtbBranchB"
+
+# misc
+# doOsdnUp=$FolderUp
+# doSFUp=$FolderUp
+ 
+
+CloneKernel "--depth=1"
+CloneCompiledGccThirteen
+CloneDTCClang
+# DisableMsmP
+DisableThin
+# EnableRELR
+# UseGoldBinutils="m"
+UseOBJCOPYBinutils="y"
+EnableRELR
+CompileClangKernelLLVMB && CleanOut
