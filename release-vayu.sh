0e00596fd26647e2ba6d631d5d32027cb7f25761
ZyCromerZ <neetroid97@gmail.com>
2023-01-28 04:09:56 +0700

    prepare full lto
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- release-vayu.sh
+++ release-vayu.sh
@@ -3,3 +3,7 @@
 . pushg/vayu.sh doa vayu-r-oss-f-c vayu-r-oss-f-d vayu-r-oss-g-c && \
 . pushg/vayu.sh dob vayu-r-oss-g-d vayu-r-oss-h-c vayu-r-oss-h-d && \
 . pushg/vayu.sh doc vayu-r-oss-i-c vayu-r-oss-i-d
+
+# vayu-r-oss-f-e vayu-r-oss-f-f
+# vayu-r-oss-g-e vayu-r-oss-g-f
+# vayu-r-oss-h-e vayu-r-oss-h-f
