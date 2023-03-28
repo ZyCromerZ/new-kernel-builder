4efec0f3954b1f17e20629f82efc502da121ca58
ZyCromerZ <neetroid97@gmail.com>
2023-03-28 07:18:04 +0700

    fix begonia
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- device/begonia-q-oss.sh
+++ device/begonia-q-oss.sh
@@ -14,6 +14,7 @@ ExFolder="Bego-Q-kernels-release"
 AnyKernelBranch="master-begonia-q-oss"
 FirstSendInfoLink="N"
 TypeBuildFor="Q-OSS"
+DoSubModules="y"
 
 # pullALmk(){
 #     [[ "$(pwd)" != "${KernelPath}" ]] && cd "${KernelPath}"
